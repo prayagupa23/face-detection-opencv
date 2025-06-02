38910698274ef60000898a409ea2f1c0ad8c5513
Prayag <prayagupadhyaya23@gmail.com>
2025-06-02 13:44:05 +0530

    Initial commit

--- facedetect.py
+++ facedetect.py
@@ -1,3 +1,10 @@
-#face detection using opencv-python
 import cv2 as cv
-# print(cv.__version__)
+import sys
+
+img = cv.imread('starry_night.jpg') # returns a numpy array
+cv.imshow("Sample window",img) # displays the image loaded by imread. Needs a string, image array data
+if img is None:
+    sys.exit("Could not find the image")
+key = cv.waitKey(0)
+if key == ord("k"):
+    cv.imwrite("starry_night.jpg",img) # writes the image(modified/same) back to computer storage
